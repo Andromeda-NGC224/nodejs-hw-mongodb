2d3d458d1cca6ca1f46af597aa5106ad0b5414a8
Andromeda-NGC224 <shadim115115@gmail.com>
2024-07-06 14:08:36 +0300

    fixing mail length

--- src/validation/contactsJoiValid.js
+++ src/validation/contactsJoiValid.js
@@ -9,7 +9,7 @@ export const contactsAddJoiValid = Joi.object({
     'any.required': 'Поле з імям - обовязкове',
   }),
   phoneNumber: Joi.string().min(3).max(20).required(),
-  email: Joi.string().min(3).max(20),
+  email: Joi.string().min(3),
   isFavourite: Joi.boolean(),
   contactType: Joi.string().valid(...contactType),
 })
@@ -21,7 +21,7 @@ export const contactsPatchJoiValid = Joi.object({
     'string.max': 'Імя повинно повинно мати не більше 20 символів',
   }),
   phoneNumber: Joi.string().min(3).max(20),
-  email: Joi.string().min(3).max(20),
+  email: Joi.string().min(3),
   isFavourite: Joi.boolean(),
   contactType: Joi.string().valid(...contactType),
 })
--- src/validation/userJoiValid.js
+++ src/validation/userJoiValid.js
@@ -8,11 +8,11 @@ export const userSignupJoi = Joi.object({
     'string.max': 'Імя повинно повинно мати не більше 20 символів',
     'any.required': 'Поле з імям - обовязкове',
   }),
-  email: Joi.string().pattern(emailRegex).min(3).max(20).required(),
+  email: Joi.string().pattern(emailRegex).min(3).required(),
   password: Joi.string().min(3).max(20).required(),
 })
 
 export const userSigninJoi = Joi.object({
-  email: Joi.string().pattern(emailRegex).min(3).max(20).required(),
+  email: Joi.string().pattern(emailRegex).min(3).required(),
   password: Joi.string().required(),
 })
